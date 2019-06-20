fb47a8b419480a700368c176bc1d5d7e3393b98d
Danielle Lancashire <dani@builds.terrible.systems>
2019-06-20 19:03:19 +0200

    pipe: Add ListenOnlyPipe

--- pipe.go
+++ pipe.go
@@ -429,6 +429,28 @@ type PipeConfig struct {
 	OutputBufferSize int32
 }
 
+// ListenOnlyPipe creates a listener on a Windows named pipe path, e.g. \\.\pipe\mypipe.
+// The pipe must already exist.
+func ListenOnlyPipe(path string, c *PipeConfig) (net.Listener, error) {
+	if c == nil {
+		c = &PipeConfig{}
+	}
+	h, err := makeServerPipeHandle(path, nil, c, false)
+	if err != nil {
+		return nil, err
+	}
+	l := &win32PipeListener{
+		firstHandle: h,
+		path:        path,
+		config:      *c,
+		acceptCh:    make(chan (chan acceptResponse)),
+		closeCh:     make(chan int),
+		doneCh:      make(chan int),
+	}
+	go l.listenerRoutine()
+	return l, nil
+}
+
 // ListenPipe creates a listener on a Windows named pipe path, e.g. \\.\pipe\mypipe.
 // The pipe must not already exist.
 func ListenPipe(path string, c *PipeConfig) (net.Listener, error) {
