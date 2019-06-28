1b3c84ab40eef2c6b14c039f083dac97a3f3f2ab
Danielle Lancashire <dani@builds.terrible.systems>
2019-06-28 14:03:17 +0200

    pipe: Add test to cover multiple readers

--- pipe_test.go
+++ pipe_test.go
@@ -218,6 +218,54 @@ func TestCloseAbortsListen(t *testing.T) {
 	}
 }
 
+func TestMultiListener(t *testing.T) {
+	l1, err := ListenPipe(testPipeName, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l1.Close()
+
+	l2, err := ListenOnlyPipe(testPipeName, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l2.Close()
+
+	ch1 := make(chan int)
+	go server(l1, ch1)
+
+	ch2 := make(chan int)
+	go server(l2, ch2)
+
+	c, err := DialPipe(testPipeName, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	rw := bufio.NewReadWriter(bufio.NewReader(c), bufio.NewWriter(c))
+	_, err = rw.WriteString("hello world\n")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = rw.Flush()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s, err := rw.ReadString('\n')
+	if err != nil {
+		t.Fatal(err)
+	}
+	ms := "got hello world\n"
+	if s != ms {
+		t.Errorf("expected '%s', got '%s'", ms, s)
+	}
+
+	<-ch1
+	<-ch2
+}
+
 func ensureEOFOnClose(t *testing.T, r io.Reader, w io.Closer) {
 	b := make([]byte, 10)
 	w.Close()
